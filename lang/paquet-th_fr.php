4f9f216b739f716cc5040df5080e479df67e94cc
Camille Lafitte <camille.lafitte@webelys.com>
2014-04-25 13:51:10 +0200

    Paquet.xml : La description est une chaine de langue
    * On déplace la description de plugin.xml selon le normalisme de paquet.xml

---
+++ lang/paquet-th_fr.php
@@ -0,0 +1,8 @@
+<?php
+    if (!defined('_ECRIRE_INC_VERSION')) return;
+
+    $GLOBALS[$GLOBALS['idx_lang']] = array(
+
+        'th_description' => "Un plugin pour l'installation simplifiée des thematiques de [laclasse.com->http://www.laclasse.com]"
+    );
+?>
