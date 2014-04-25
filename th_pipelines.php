11a2f4dec83d2c0e853e0314c93a8190d4f50a58
Camille Lafitte <camille.lafitte@webelys.com>
2014-04-25 18:04:26 +0200

    Nettoyage de jquery-ui
    * On profite du plugin jquery-ui pour faire les chargements des scripts (on profite ainsi de la compression)
    * On retire les vieux scripts inutiles (1.8, 1.10) et les appels dans le html
    * Comme spip3 ne fournie pas encore la version 1.10 de jquery on surcharge spécifiquement pour le theme sq-page les fichiers js.

--- th_pipelines.php
+++ th_pipelines.php
@@ -29,4 +29,17 @@ if (!defined("_ECRIRE_INC_VERSION")) return;
 		return $boucle;
 	}
 
+    function th_jqueryui_plugins($scripts){
+        $scripts[] = "jquery.ui.core";
+        $scripts[] = "jquery.ui.widget";
+        $scripts[] = "jquery.ui.mouse";
+        $scripts[] = "jquery.ui.position";
+        $scripts[] = "jquery.ui.draggable";
+        $scripts[] = "jquery.ui.droppable";
+        $scripts[] = "jquery.ui.tooltip"; /* a TROUVER */
+        $scripts[] = "jquery.ui.effect"; 
+        $scripts[] = "jquery.ui.effect-bounce";
+                                                
+        return $scripts;
+    }
 ?>
