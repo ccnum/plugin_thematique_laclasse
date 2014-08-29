547744cd627861b571be215c772326a42357cfc9
Camille <camille.lafitte@webelys.com>
2014-08-29 15:49:15 +0200

    Le statut ENT est une liste prédéfinie
    * Ne proposer à la modification qu'une liste restreinte de choix

--- base/th_cextras.php
+++ base/th_cextras.php
@@ -14,12 +14,13 @@ function th_declarer_champs_extras($champs = array()) {
         ),
     );
     $champs['spip_auteurs']['ent_statut'] = array(
-        'saisie' => 'input',//Type du champ (voir plugin Saisies)
+        'saisie' => 'selection',//Type du champ (voir plugin Saisies)
         'options' => array(
         'nom' => 'ent_statut',
         'label' => _T('th:ent_statut'),
         'sql' => "varchar(255) NOT NULL DEFAULT ''",
-        'defaut' => '',// Valeur par défaut
+        'defaut' => 'ELEVE',// Valeur par défaut
+        'datas' => array('ADM','ADM_ETB','PROF','PARENT','ELEVE'),
         'restrictions'=>array('voir' => array('auteur' => ''),//Tout le monde peut voir
         'modifier' => array('auteur' => 'webmestre')),//Seuls les webmestres peuvent modifier
         ),
