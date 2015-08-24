ab4f56d429a7d33056d9338b164cb3049bd4ee95
Patrick Vincent <pvincent@erasme.org>
2015-08-24 16:06:15 +0200

    Ent_role devient un champ ouvert pour compatibilité v2+v3 laclasse.com

--- base/th_cextras.php
+++ base/th_cextras.php
@@ -9,19 +9,18 @@ function th_declarer_champs_extras($champs = array()) {
         'label' => _T('th:ent'),
         'sql' => "varchar(255) NOT NULL DEFAULT ''",
         'defaut' => '',// Valeur par défaut
-        'disable' => 'disable',
+        //'disable' => 'disable',
         'restrictions'=>array('voir' => array('auteur' => ''),//Tout le monde peut voir
         'modifier' => array('auteur' => 'webmestre')),//Seuls les webmestres peuvent modifier
         ),
     );
     $champs['spip_auteurs']['ent_statut'] = array(
-        'saisie' => 'selection',//Type du champ (voir plugin Saisies)
+        'saisie' => 'input',//Type du champ (voir plugin Saisies)
         'options' => array(
         'nom' => 'ent_statut',
         'label' => _T('th:ent_statut'),
         'sql' => "varchar(255) NOT NULL DEFAULT ''",
         'defaut' => '',// Valeur par défaut
-        'datas' => array('ADM' => 'ADM','ADM_ETB' => 'ADM_ETB','PROF' => 'PROF' ,'PARENT' => 'PARENT' ,'ELEVE' => 'ELEVE'),
         'restrictions'=>array('voir' => array('auteur' => ''),//Tout le monde peut voir
         'modifier' => array('auteur' => array('webmestre','0minirezo'))),//Seuls les webmestres peuvent modifier
         ),
