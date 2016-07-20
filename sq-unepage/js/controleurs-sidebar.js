3fd436fdc5ebb19906d429cf161b5b4f72ee07b7
kevinvennitti <kevin.vennitti@gmail.com>
2016-07-20 15:57:36 +0200

    Documentation de controleurs-sidebar.js

--- sq-unepage/js/controleurs-sidebar.js
+++ sq-unepage/js/controleurs-sidebar.js
@@ -1,9 +1,19 @@
+/**
+ * Charge l'URL dans la sidebar principale.
+ *
+ * @param {string} url - URL de la page à charger avec AJAX
+ * @param {string} typePage - Type du contenu SPIP : <tt>article</tt>, <tt>rubrique</tt>…
+ * @param {string} typeObjet - Type de l'objet principal de la page : <tt>consignes</tt>, <tt>travail_en_cours</tt>, <tt>blogs</tt>, <tt>evenements</tt>, <tt>ressources</tt>, <tt>classes</tt>…
+ *
+ * @see loadContentInLateralSidebar
+ *
+ * @todo Loading et son callback
+ */
+
 function loadContentInMainSidebar(url, typePage, typeObjet) {
   console.log('%c Main'+' %c '+url+' ', 
               'background:#8BC34A;color:#fff;padding:2px;border-radius:2px;', 
               'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
-  
-  // TODO : loading ?
   
   $('#sidebar_main_inner').load(url, function() {
     showSidebar();
@@ -14,16 +24,25 @@ function loadContentInMainSidebar(url, typePage, typeObjet) {
                 'background:#8BC34A;color:#fff;padding:2px;border-radius:2px;', 
                 'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
   });
-  
-  // TODO : callback /loading ?
 }
+
+
+/**
+ * Charge l'URL dans la sidebar secondaire.
+ *
+ * @param {string} url - URL de la page à charger avec AJAX
+ * @param {string} typePage - Type du contenu SPIP : <tt>article</tt>, <tt>rubrique</tt>…
+ * @param {string} typeObjet - Type de l'objet principal de la page : <tt>consignes</tt>, <tt>travail_en_cours</tt>, <tt>blogs</tt>, <tt>evenements</tt>, <tt>ressources</tt>, <tt>classes</tt>…
+ *
+ * @see loadContentInMainSidebar
+ *
+ * @todo Loading et son callback
+ */
 
 function loadContentInLateralSidebar(url, typePage, typeObjet) {
   console.log('%c Lateral'+' %c '+url+' ', 
               'background:#FFA000;color:#fff;padding:2px;border-radius:2px;', 
               'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
-  
-  // TODO : loading ?
   
   $('#sidebar_lateral_inner').load(url, function(response, status, xhr){
     if ( status == "error" ) {
@@ -38,17 +57,36 @@ function loadContentInLateralSidebar(url, typePage, typeObjet) {
                 'background:#FFA000;color:#fff;padding:2px;border-radius:2px;', 
                 'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
   });
-  
-  
-  // TODO : callback /loading ?
 }
+
+
+/**
+ * Affiche la sidebar secondaire.
+ *
+ * @see loadContentInLateralSidebar
+ */
 
 function showSidebarLateral() {
   $('body').addClass('hasSidebarLateralVisible');
 }
+
+
+/**
+ * Masque la sidebar secondaire.
+ *
+ * @see loadContentInLateralSidebar
+ */
+ 
 function hideSidebarLateral() {
   $('body').removeClass('hasSidebarLateralVisible');
 }
+
+
+/**
+ * Affiche la sidebar principale.
+ *
+ * @see loadContentInMainSidebar
+ */
 
 function showSidebar() {
   $('body').addClass('hasSidebarOpen');
