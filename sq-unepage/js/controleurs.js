3a5cb163b5c28bf6b48449e25b8f765b200cdc05
kevinvennitti <kevin.vennitti@gmail.com>
2016-07-25 14:24:36 +0200

    Factorisation des fichiers JS

--- sq-unepage/js/controleurs.js
+++ sq-unepage/js/controleurs.js
@@ -1,3 +1,106 @@
+var vue = 'timeline';
+
+
+
+$().ready(function(){
+  $('#timeline_fixed').on('click', function(){
+  	CCN.projet.showWholeTimeline();
+  })
+      
+      
+  $('#sidebarExpand').on('click', function(){
+    if ($('body').hasClass('hasSidebarExpanded')) {
+      $('body').removeClass('hasSidebarExpanded');
+      
+      if ($('body').hasClass('hadSidebarLateralVisible')) {
+        $('body').removeClass('hadSidebarLateralVisible');
+        showSidebarLateral();
+      }
+    } else {
+      $('body').addClass('hasSidebarExpanded');
+      
+      if ($('body').hasClass('hasSidebarLateralVisible')) {
+        $('body').addClass('hadSidebarLateralVisible');
+      }
+      
+      hideSidebarLateral();
+    }
+  });
+  
+  $('#sidebarCache').on('click', function(){
+    $('body').removeClass('hasSidebarExpanded');
+  });
+  
+  $("#menu_bas ul a").not('#menu-classes-select ul a').tooltip({
+    position: {
+      my: "center bottom-4",
+      at: "center top",
+      using: function( position, feedback ) {
+        $( this ).css( position );
+        $( "<div>" )
+          .addClass( "arrow" )
+          .addClass( feedback.vertical )
+          .addClass( feedback.horizontal )
+          .appendTo( this );
+      },
+      collision: "fit",
+    },
+    show: {
+      duration: 100,
+      effect: 'fadeIn'
+    }
+    ,
+    hide: {
+      duration: 100,
+      effect: 'fadeOut'
+    }          
+  });
+
+  // Click des logos-menus
+  // Ouverture Timeline
+  
+  $( "ul.vue a.logo_menu-timeline" ).click(function() {
+    $('#zone_classe').stop().fadeOut(1000);
+    $('#zone').stop().fadeIn(1000);
+    $('#menu-classe').stop().fadeOut(1000);
+    $('#menu_bas a[data-filter-value=".ressource_consignes"]').parent().stop().fadeOut(0);
+    $('#menu_bas a[data-filter-value=".ressource_reponses"]').parent().stop().fadeOut(0);
+    $('#menu_bas a[data-filter-value=".ressource_classes"]').parent().stop().fadeIn(1000);
+    $('#menu_bas a[data-filter-value=".ressource_agora"]').parent().stop().fadeIn(1000);
+    
+    vue = 'timeline';
+  });
+
+  // Ouverture Classes
+  
+  $( "ul.vue a.logo_menu-classe" ).click(function() {
+    $('#zone_classe').stop().fadeIn(1000,function(){
+      $('#classes').isotope({ layoutMode : 'masonry' });
+    });
+    $('#zone').stop().fadeOut(1000);
+    $('#menu-classe').stop().fadeIn(1000);
+    $('#menu_bas a[data-filter-value=".ressource_classes"]').parent().stop().fadeOut(0);
+    $('#menu_bas a[data-filter-value=".ressource_agora"]').parent().stop().fadeOut(0);
+    $('#menu_bas a[data-filter-value=".ressource_consignes"]').parent().stop().fadeIn(1000);
+    $('#menu_bas a[data-filter-value=".ressource_reponses"]').parent().stop().fadeIn(1000);
+    
+    if (($('#menu_bas a[data-filter-value=".ressource_consignes"]').hasClass('selected')) 
+     || ($('#menu_bas a[data-filter-value=".ressource_reponses"]').hasClass('selected'))) {
+       // isotope ressources ferme tout
+    }
+    
+    vue = 'classes';
+  });
+
+  // Initialisation des logos-menus
+  
+  $("#menu_bas li.logo a.selected").each(function (){ changeCouleurLogoMenu(this); });
+  $('#menu_bas a[data-filter-value=".ressource_consignes"]').parent().stop().fadeOut(1000);
+  $('#menu_bas a[data-filter-value=".ressource_reponses"]').parent().stop().fadeOut(1000);
+});
+
+
+
 /**
  * Définit la largeur de la zone.
  */
@@ -306,29 +409,6 @@ function createReponse(id_consigne, id_rubrique_classe, numero){
 
 
 /**
- * Appelle le chargement de la ressource
- * dans la sidebar principale et appelle
- * (…)
- *
- * @see loadContentInMainSidebar
- * @see loadContentInLateralSidebar
- *
- * @todo Modifier le contenu de la sidebar principale
- * @todo Modifier le contenu de la sidebar secondaire
- * @todo Documenter
- */
-
-function ressources_click(){
-	if ($('#zone_classe').is(':hidden'))	{
-		
-		var url = CCN.projet.url_popup_ressources;
-		popup(url,'edition');
-    console.log('ressources_click');
-	}	
-}
-
-
-/**
  * Appelle le chargement de l'agora
  * dans la sidebar principale et appelle
  * (…)
@@ -344,7 +424,6 @@ function ressources_click(){
 function callAgora(){
 	if ($('#zone_classe').is(':hidden'))	{
 		
-		//popup('spip.php?page=rubrique&id_rubrique=33&mode=detail&type_objet=ressources','agora');
 		var url = CCN.projet.url_popup_agora;
 		popup(url,'agora');
 		console.log('callAgora');
@@ -429,4 +508,155 @@ function updateConnecteurs() {
     })
     .width(parseFloat(length)+'px');
   });
+}
+
+
+/**
+ * Change la couleur du bouton une fois cliqué.
+ *
+ * @param {string} val - Sélecteur de l'élément DOM en jQuery
+ * @todo Améliorer la récupération de la couleur ?
+ */
+
+function changeCouleurLogoMenu(val){
+  var color = $(val).css('background-color');
+  $(val).parent().parent().parent().children("h3").css('background-color',color);
+}
+
+
+/**
+ * @deprecated
+ */
+
+function popup(url,mode){
+  console.log('%c Popup (insertion jQuery désactivée, préférer `{type_objet}_click`)'+' %c '+url+' ', 
+              'background:tomato;color:#fff;padding:2px;border-radius:2px;', 
+              'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
+}
+
+
+/**
+ * Met à jour le cookie et recharge la page.
+ *
+ * @param {string} url - URL de la page de destination
+ * @param {string} cookie_nom - Nom du cookie
+ * @param {string} cookie_valeur - Valeur du cookie
+ */
+
+function reloadAndSetCookie(url,cookie_nom,cookie_valeur) {
+  document.cookie = cookie_nom + "=" + escape(cookie_valeur);
+  reload(url);
+}
+
+
+/**
+ * Gère le rechargement de la page.
+ *
+ * @param {string} url - URL de la page à charger avec AJAX ou <tt>self</tt> pour recharger la même page
+ *
+ * @see reloadAndSetCookie
+ */
+
+function reload(url) {
+  if (url == 'self')  {
+    location.reload( true );
+    window.location.reload();
+  }
+  else window.location.href = url;
+}
+
+/**
+ * Charge l'URL dans la sidebar principale.
+ *
+ * @param {string} url - URL de la page à charger avec AJAX
+ * @param {string} typePage - Type du contenu SPIP : <tt>article</tt>, <tt>rubrique</tt>…
+ * @param {string} typeObjet - Type de l'objet principal de la page : <tt>consignes</tt>, <tt>travail_en_cours</tt>, <tt>blogs</tt>, <tt>evenements</tt>, <tt>ressources</tt>, <tt>classes</tt>…
+ *
+ * @see loadContentInLateralSidebar
+ *
+ * @todo Loading et son callback
+ */
+
+function loadContentInMainSidebar(url, typePage, typeObjet) {
+  console.log('%c Main'+' %c '+url+' ', 
+              'background:#8BC34A;color:#fff;padding:2px;border-radius:2px;', 
+              'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
+  
+  $('#sidebar_main_inner').load(url, function() {
+    showSidebar();
+    hideSidebarLateral();
+    $('#sidebar_content').scrollTop(0);
+      
+    console.log('%c Main'+' %c Loaded ', 
+                'background:#8BC34A;color:#fff;padding:2px;border-radius:2px;', 
+                'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
+  });
+}
+
+
+/**
+ * Charge l'URL dans la sidebar secondaire.
+ *
+ * @param {string} url - URL de la page à charger avec AJAX
+ * @param {string} typePage - Type du contenu SPIP : <tt>article</tt>, <tt>rubrique</tt>…
+ * @param {string} typeObjet - Type de l'objet principal de la page : <tt>consignes</tt>, <tt>travail_en_cours</tt>, <tt>blogs</tt>, <tt>evenements</tt>, <tt>ressources</tt>, <tt>classes</tt>…
+ *
+ * @see loadContentInMainSidebar
+ *
+ * @todo Loading et son callback
+ */
+
+function loadContentInLateralSidebar(url, typePage, typeObjet) {
+  console.log('%c Lateral'+' %c '+url+' ', 
+              'background:#FFA000;color:#fff;padding:2px;border-radius:2px;', 
+              'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
+  
+  $('#sidebar_lateral_inner').load(url, function(response, status, xhr){
+    if ( status == "error" ) {
+      var msg = "Sorry but there was an error: ";
+      console.log( msg + xhr.status + " " + xhr.statusText );
+    }
+    
+    $('#sidebar_content').scrollTop(0);
+    showSidebar();
+      
+    console.log('%c Lateral'+' %c Loaded ', 
+                'background:#FFA000;color:#fff;padding:2px;border-radius:2px;', 
+                'background:#009688;color:#fff;padding:2px;display:block;margin-top:5px;border-radius:2px;');
+  });
+}
+
+
+/**
+ * Affiche la sidebar secondaire.
+ *
+ * @see loadContentInLateralSidebar
+ */
+
+function showSidebarLateral() {
+  $('body').addClass('hasSidebarLateralVisible');
+}
+
+
+/**
+ * Masque la sidebar secondaire.
+ *
+ * @see loadContentInLateralSidebar
+ */
+ 
+function hideSidebarLateral() {
+  $('body').removeClass('hasSidebarLateralVisible');
+}
+
+
+/**
+ * Affiche la sidebar principale.
+ *
+ * @see loadContentInMainSidebar
+ */
+
+function showSidebar() {
+  $('body').addClass('hasSidebarOpen');
+  $('#sidebar').addClass('show');
+  updateTimeline();
 }
