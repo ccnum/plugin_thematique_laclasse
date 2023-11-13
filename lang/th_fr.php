7ff869cd835642a8d596928ae8754adede2fbd74
Pierre KUHN <webmaster@pierre-kuhn.org>
2023-11-13 09:58:56 +0100

    feat: rajout de langue

---
+++ lang/th_fr.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// L
+	'legend' => 'Légende',
+
+	// L
+	'label_site_ent_nom' => "NOM du site de l'ENT",
+	'label_site_ent_url' => "URL du site de l'ENT",
+	'label_espace_doc_url' => "Url de la documentation du site de l'ENT",
+
+	// T
+	'titre_page_configurer_th' => "Configuration du plugin",
+);
