df1e932f4990165bb0484080f92872981a94e7d7
Pierre KUHN <webmaster@pierre-kuhn.org>
2023-12-05 09:57:32 +0100

    feat: code mort car les nouvelle versions de spip

--- th_pipelines.php
+++ th_pipelines.php
@@ -1,38 +1,6 @@
 <?php
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-//Pre_boucles
-//Retourne les articles et articles syndiqués en lien avec l'année scolaire
-function th_pre_boucle($boucle) {
-	$affichage = _affichage;
-
-	$annee = _annee_scolaire;
-	$mois = '08';
-	$jour = '01';
-
-	$annee2 = intval(_annee_scolaire) + 1;
-	$mois2 = '08';
-	$jour2 = '01';
-
-	/**
-	 * Commenter par Pierre KUHN le 01/12/2023 ne sais pas si cela sert
-	 */
-	/*
-	$date_debut = $annee_scolaire . "." . $mois . "." . $jour;
-	$date_fin = $annee_scolaire2 . "." . $mois2 . "." . $jour2;
-	*/
-	if (($boucle->type_requete == 'articles') || ($boucle->type_requete == 'syndic_articles')) {
-		$date = $boucle->id_table . ".date";
-		if ((!isset($boucle->modificateur['tout'])) && (!strstr($_SERVER['REQUEST_URI'], "/ecrire")) && (!$affichage == 'unepage'))
-			$boucle->where[] = array(
-				"'AND'",
-				array("'>='", "'$date'", ("'\"$annee-$mois-$jour\"'")),
-				array("'<='", "'$date'", ("'\"$annee2-$mois2-$jour2\"'"))
-			);
-	}
-	return $boucle;
-}
-
 function th_jqueryui_plugins($scripts) {
 	$scripts[] = "jquery.ui.core";
 	$scripts[] = "jquery.ui.widget";
